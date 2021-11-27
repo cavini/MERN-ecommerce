86c445e3b5ba37c34f543e0d4c45c81da2cdd437
Arthur Cavini <arthurcavini@hotmail.com>
2021-11-27 15:11:36 -0300

    feature: created models for database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './data/products.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -19,5 +22,10 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+  )
+);
