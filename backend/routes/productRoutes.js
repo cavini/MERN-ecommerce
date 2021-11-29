1be5c9a6cc3b4a036a3709ab62ba4be9edb3dcba
Arthur Cavini <arthurcavini@hotmail.com>
2021-11-29 12:45:25 -0300

    fix: reverted changes from trycatch to express async handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,35 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
 import Product from '../models/productModel.js';
 
-const router = express.Router();
-
-// @description Fetch all products
-// @route GET /api/products
-// @access Public
-
-router.get('/', async (req, res) => {
-  try {
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({});
+
     res.json(products);
-  } catch (error) {
-    console.error(error);
-  }
-});
+  })
+);
 
-// @description Fetch single product
-// @route GET /api/products/:id
-// @access Public
-
-router.get('/:id', async (req, res) => {
-  try {
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({
-        message: 'Product not found',
-      });
+      res.status(404);
+      throw new Error('Product not found');
     }
-  } catch (error) {
-    console.error(error);
-  }
-});
+  })
+);
 
 export default router;
